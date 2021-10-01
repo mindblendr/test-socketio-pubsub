6a00241a888889c5819d067922619ccfcf2a5f7c
Mindblendr <mindblendr@gmail.com>
2021-10-01 09:33:43 +0800

    added a page

--- server.js
+++ server.js
@@ -7,12 +7,20 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 const redis = require('socket.io-redis');
-io.adapter(redis({ host: process.env.REDIS_PUBSUB_HOST, port: process.env.REDIS_PUBSUB_PORT }));
+try {
+    io.adapter(redis({ host: process.env.REDIS_PUBSUB_HOST, port: process.env.REDIS_PUBSUB_PORT, timeout: 5000 }));
+} catch (error) {
+    console.log(error);
+}
 
 io.on('connection', (socket) => {
     socket.on('send-message', (payload) => {
         io.emit('receive-message', payload);
     });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 });
 
 
